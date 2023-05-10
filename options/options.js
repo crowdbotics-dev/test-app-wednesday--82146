0614c6b69ac39b41479421159ccc7639c9f47b82
Hesbon Kiptoo <hesbon.kiptoo@crowdbotics.com>
2023-05-10 18:46:49 +0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "test-app-wednesday--82146",
   url: "https://test-app-wednesday--82146.botics.co",
   api: "https://test-app-wednesday--82146.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
